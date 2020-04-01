a353a6aef01f7a19455a249c3acd9ec4ef3be8f8
Anders <idris.maps@gmx.ch>
2020-04-01 22:01:52 +0200

    minify prism css

---
+++ scripts/minifyPrismCss.ts
@@ -0,0 +1,28 @@
+import { readdir, writeFile, readFile } from 'fs'
+import { promisify } from 'util'
+import { resolve } from 'path'
+import { exec } from 'child_process'
+
+const bash = (cmd: string) => new Promise((resolve, reject) =>
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) { return reject(err) }
+    if (stderr) { return reject(stderr) }
+    resolve()
+  })
+)
+const pathToSrc = resolve(__dirname, '..', 'node_modules', 'prismjs', 'themes')
+const pathToAssets = resolve(__dirname, '..', 'assets')
+
+const getFiles = () =>
+  promisify(readdir)(pathToSrc)
+
+const minify = (file: string) =>
+  bash(`npx uglifycss ${resolve(pathToSrc, file)} > ${resolve(pathToAssets, file)}`)
+
+const run = async () => {
+  const files = await getFiles()
+  await Promise.all(files.map(minify))
+  console.log('Done minifying prism.css')
+}
+
+run()
