2e888b3ed7cc4dadb28cc7cb56b923abc10e5150
Anders <idris.maps@gmx.ch>
2020-04-02 20:41:36 +0200

    Add bullshit score

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,18 @@ import parseCmd, { Config } from './utils/parseCmd'
 import parseMd from './utils/parseMd'
 import { read, write } from './utils/files'
 import renderHTML from './render'
-import { replaceBullshit } from './bullshit'
+import { replaceBullshit, countBullshit } from './bullshit'
 
 const config = parseCmd()
+
+const bullShitScore = (md: string) => {
+  const { words, bullshit, score } = countBullshit(md)
+  return `
+    Your bullshit score is: ${score}
+    
+    ${bullshit} out of ${words} words are bullshit.
+  `
+}
 
 const run = async ({ theme, replaceBs, file, output }: Config) => {
   try {
@@ -12,6 +21,7 @@ const run = async ({ theme, replaceBs, file, output }: Config) => {
     const slides = parseMd(replaceBs ? replaceBullshit(md) : md)
     const html = await renderHTML(slides, theme)
     await write(output, html)
+    console.log(bullShitScore(md))
   } catch (e) {
     console.log(e)
   }
